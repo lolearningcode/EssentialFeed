45ce09df3022552f5c4c59f31aedb02f43ebd89f
lolearningcode <lolearningcode@gmail.com>
2022-04-06 22:04:55 -0700

    created FeedLoader boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Teddy Be Goin Hard on 4/6/22.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
+
