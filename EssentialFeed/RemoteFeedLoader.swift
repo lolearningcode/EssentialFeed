cd01b74f821e5d1f600969772bade963a3048c59
lolearningcode <lolearningcode@gmail.com>
2022-04-10 07:45:01 -0700

    replaced hardcoded number with variable for context

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -68,8 +68,10 @@ private class FeedItemsMapper {
         }
     }
     
+    static var Ok_200: Int { return 200 }
+    
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == Ok_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         
