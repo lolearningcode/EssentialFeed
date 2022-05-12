a3fc4110c20ff493f369c507ac8842998efc7cfa
lolearningcode <lolearningcode@gmail.com>
2022-05-11 17:23:10 -0700

    Tested cache loading on days less than or more than 7

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -10,6 +10,8 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
+
     
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
@@ -32,13 +34,26 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error = error {
+        store.retrieve { [unowned self] result in
+            switch result {
+            case let .found(feed, timestamp) where self.validate(timestamp):
+                completion(.success(feed.toModels()))
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            case .found, .empty:
                 completion(.success([]))
             }
         }
+    }
+    
+    private var maxCachedAge: Int {
+        return 7
+    }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCachedAge = calendar.date(byAdding: .day, value: maxCachedAge, to: timestamp) else { return false }
+        
+        return currentDate() < maxCachedAge
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
@@ -53,5 +68,11 @@ public final class LocalFeedLoader {
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
