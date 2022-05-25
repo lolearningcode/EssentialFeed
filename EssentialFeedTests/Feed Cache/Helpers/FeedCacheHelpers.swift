99955cb1eb5e17d8cdfa7b34cd213840704e59af
lolearningcode <lolearningcode@gmail.com>
2022-05-25 16:45:29 -0700

    Added global helper file

---
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheHelpers.swift
@@ -0,0 +1,38 @@
+//
+//  FeedCacheHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Cleo Howard on 5/25/22.
+//
+
+import Foundation
+import EssentialFeed
+
+func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (models, local)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
+
