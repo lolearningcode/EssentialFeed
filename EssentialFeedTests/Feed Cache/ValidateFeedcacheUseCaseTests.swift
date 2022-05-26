4643c0a24f16f20af7d76666710b467268dd5b66
lolearningcode <lolearningcode@gmail.com>
2022-05-25 17:08:52 -0700

    Validate cache doesNotDeleteCacheAfterSUTInstanceHasBeenDeallocated

--- EssentialFeedTests/Feed Cache/ValidateFeedcacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedcacheUseCaseTests.swift
@@ -70,6 +70,19 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
+    func test_validatecache_doesNotDeleteCacheAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        sut?.validateCache()
+        
+        sut = nil
+        
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: HELPERS
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (loader: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
